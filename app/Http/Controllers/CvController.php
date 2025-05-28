16566ac01d7d0a9a3ae33422ab090b15fd0dc382
unknown <kostantinoabate98@gmail.com>
2025-05-28 19:13:37 +0200

    up

--- app/Http/Controllers/CvController.php
+++ app/Http/Controllers/CvController.php
@@ -6,7 +6,7 @@ class CvController extends Controller
 {
     public function download(): \Symfony\Component\HttpFoundation\BinaryFileResponse
     {
-        $fullPath = storage_path('app/public/CV Developer.pdf');
+        $fullPath = storage_path('app/public/Kostantino Abate PHP Developer.pdf');
 
         if (! file_exists($fullPath)) {
             abort(404, "File non trovato");
